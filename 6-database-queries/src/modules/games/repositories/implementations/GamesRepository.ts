e6223a5e698fa49c331527689a3cf1bc49b0dd65
Judson Junior <judsonjuniorr@gmail.com>
2022-02-08 08:25:03 -0300

    database queries

---
+++ 6-database-queries/src/modules/games/repositories/implementations/GamesRepository.ts
@@ -0,0 +1,38 @@
+import { getRepository, Repository } from "typeorm";
+
+import { User } from "../../../users/entities/User";
+import { Game } from "../../entities/Game";
+
+import { IGamesRepository } from "../IGamesRepository";
+
+export class GamesRepository implements IGamesRepository {
+  private repository: Repository<Game>;
+
+  constructor() {
+    this.repository = getRepository(Game);
+  }
+
+  async findByTitleContaining(param: string): Promise<Game[]> {
+    return this.repository
+      .createQueryBuilder()
+      .where("title ilike :title", { title: `%${param}%` })
+      .getMany();
+  }
+
+  async countAllGames(): Promise<[{ count: string }]> {
+    return this.repository.query("SELECT COUNT(id) FROM games"); // Complete usando raw query
+  }
+
+  async findUsersByGameId(id: string): Promise<User[]> {
+    return this.repository
+      .createQueryBuilder("games")
+      .innerJoinAndSelect("games.users", "users")
+      .getOne()
+      .then((game) => {
+        if (!game) {
+          throw new Error("Game not found");
+        }
+        return game.users;
+      });
+  }
+}
